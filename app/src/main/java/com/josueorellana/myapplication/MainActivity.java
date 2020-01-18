efb8d73bf14a234b2814d255ccf65882740c3040
Josue Orellana <orellana.josue818@gmail.com>
2020-01-18 12:42:16 -0800

    Button code implemented

--- app/src/main/java/com/josueorellana/myapplication/MainActivity.java
+++ app/src/main/java/com/josueorellana/myapplication/MainActivity.java
@@ -2,12 +2,35 @@ package com.josueorellana.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText answerTextField;
+    private TextView answerLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        answerTextField = findViewById(R.id.answer_text_field);
+        Button submitButton = findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(this);
+        answerLabel = findViewById(R.id.answer_label);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String answerText = answerTextField.getText().toString();
+        String correctAnswer = "42";
+        if (answerText.equalsIgnoreCase(correctAnswer)) {
+            answerLabel.setText(R.string.answer_label_text_correct);
+        } else {
+            answerLabel.setText(R.string.answer_label_text_incorrect);
+        }
     }
 }
